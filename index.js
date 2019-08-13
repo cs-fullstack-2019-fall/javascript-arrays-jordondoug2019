984d9005a6729b497c1c1ee72314a7601817febf
jordondoug2019 <jdouglas2015@gmail.com>
2019-08-13 12:04:53 -0500

    Array Exercise Answers

---
+++ index.js
@@ -0,0 +1,30 @@
+// #### Exercise 1
+// // 1. Create an array.
+// // 2. Add four names to the array.
+// // 3. Print the array
+// //
+// // #### Exercise 2
+// // 1. Create an array with 5 numbers in it.
+// // 2. Print the numbers.
+// //
+// //     #### Exercise 3
+// // 1. Create an array with 4 names.
+// // 2. Print the third item in the array.
+// // 3. Delete the second item.
+// // 4. Print the third item in the array again.
+
+//Exercise 1
+let Names=["Jordon","Barbie","BigFoot","Steve"];
+
+alert(Names);
+//
+// //Exercise 2
+let Numbers=[1,2,3,4,5];
+alert(Numbers);
+
+//Exercise3
+let Names2= ["willie","joe","timon","puma"];
+console.log(Names2[2]);
+Names2.splice(1,1);
+console.log(Names2);
+console.log(Names2[2])
